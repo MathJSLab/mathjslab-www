70e427a25043b734337f6badcd72e5d613dd14c6
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2024-11-30 15:49:37 -0300

    Initial Commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./site/css");
-    eleventyConfig.addPassthroughCopy("./site/img");
+    eleventyConfig.addPassthroughCopy('./site/css');
+    eleventyConfig.addPassthroughCopy('./site/img');
     return {
         dir: {
-            input: "./site",
-            includes: "../includes",
-            data: "../data",
-            output: "www"
+            input: './site',
+            includes: '../includes',
+            data: '../data',
+            output: './www'
         },
-        pathPrefix: "/",
+        pathPrefix: '/',
     };
 };
